2b25d5b3c606f1337e658c2d57a9cca0a7c59700
Donald F. Coffin <donald.coffin@reminetworks.com>
2020-01-04 03:08:14 -0500

    Add Beer Order Enums. Closes #14

---
+++ src/main/java/guru/sfg/beer/order/service/domain/BeerOrderEventEnum.java
@@ -0,0 +1,10 @@
+package guru.sfg.beer.order.service.domain;
+
+/**
+ * Created by Donald F. Coffin on 01/04/2020 at 02:56
+ */
+public enum BeerOrderEventEnum {
+    VALIDATE_ORDER, VALIDATION_PASSED, VALIDATION_FAILED,
+    ALLOCATION_SUCCESS, ALLOCATION_NO_INVENTORY, ALLOCATION_FAILED,
+    BEERORDER_PICKED_UP
+}
